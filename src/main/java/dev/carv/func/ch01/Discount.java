00a950b87379b3b7f2cd674bca32b8ea069e8cc4
carolusquintus <carloschars@gmail.com>
2024-08-26 22:01:37 -0600

    Discount interface with RandomGenerator

--- src/main/java/dev/carv/func/ch01/Discount.java
+++ src/main/java/dev/carv/func/ch01/Discount.java
@@ -1,21 +1,16 @@
 package dev.carv.func.ch01;
 
 import com.github.javafaker.Faker;
+import dev.carv.func.RandomGenerator;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.IntStream;
 
-public interface Discount {
+public interface Discount extends RandomGenerator {
 
     default List<Double> generatePrices() {
         var faker = new Faker(Locale.US);
-        var prices = new ArrayList<Double>();
-
-        IntStream.range(0, 20).forEach(i -> prices.add(Double.valueOf(faker.commerce().price())));
-
-        return prices;
+        return generateList(20, () -> Double.valueOf(faker.commerce().price()));
     }
 
     Double calculate(List<Double> prices);
